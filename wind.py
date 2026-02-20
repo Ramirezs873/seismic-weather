7facbb7a7f23c31e95dd54604562184257bb1d79
Ramirezs873 <ramirezs@utas.edu.au>
2026-02-20 14:13:42 +1100

    20.02.26 2:12pm Update
    Tweaks to plot_wind_speed()
    Added plot_rose_wind()

--- wind.py
+++ wind.py
@@ -45,6 +45,7 @@ def read_data(path,
 
     return df
 
+
 def plot_wind_speed(data, year, month):
 
     """
@@ -56,33 +57,77 @@ def plot_wind_speed(data, year, month):
         month (int): The month for plotting (1-12).
     """
 
-    df_year_month = {}
-    for i in range(2010, 2026):
-        for k in range(1, 13):
+    df_slice = data[(data['datetime'].dt.year == year) &
+                    (data['datetime'].dt.month == month)].copy()
 
-            df_slice = data[(data['datetime'].dt.year == i) &
-                            (data['datetime'].dt.month == k)].copy()
+    if df_slice.empty:
+        print(f"No data available for {year}-{month:02d}")
+        return
 
-            if df_slice.empty:
-                continue
+    # Convert to numeric
+    df_slice['Wind speed in km/h'] = pd.to_numeric(df_slice['Wind speed in km/h'], 
+                                                    errors='coerce')
+    wind_speed = df_slice['Wind speed in km/h']
+    df_slice['Wind_norm'] = wind_speed / wind_speed.max()
 
-            # Convert to numeric
-            df_slice['Wind speed in km/h'] = pd.to_numeric(df_slice['Wind speed in km/h'], 
-                                                           errors='coerce')
-            wind_speed = df_slice['Wind speed in km/h']
-            df_slice['Wind_norm'] = wind_speed / wind_speed.max()
-
-            df_year_month[(i, k)] = df_slice
-
-    
     plt.figure(figsize=(15,6))
-    plt.plot(df_year_month[year, month]['datetime'], df_year_month[year, month]['Wind speed in km/h'], 
+    plt.plot(df_slice['datetime'], df_slice['Wind speed in km/h'], 
             color='black', linewidth=0.5)
-    ymax = np.max(df_year_month[year, month]['Wind speed in km/h'])
-    plt.title(f'Wind Speed at Station:{df_year_month[year, month]["Station Number"].iloc[0]} @ {year}-{month:02d}')
+    ymax = np.max(df_slice['Wind speed in km/h'])
+    plt.title(f'Wind Speed at Station:{df_slice["Station Number"].iloc[0]} @ {year}-{month:02d}')
     plt.ylabel('Wind Speed (km/h)')
     plt.xlabel('Year')
     plt.ylim(0, ymax + 10)
     plt.grid(alpha=0.3)
     plt.tight_layout()
     plt.show()
+
+def plot_rose_wind(data, year, month):
+
+    """
+    Plots a rose plot for wind speed and direction for a given year and month from the provided DataFrame.
+
+    Parameters:
+        data (pd.DataFrame): The DataFrame containing the weather station data.
+        year (int): The year for plotting (2010-2025).
+        month (int): The month for plotting (1-12).
+    """
+
+    
+    df_slice = data[(data['datetime'].dt.year == year) &
+                    (data['datetime'].dt.month == month)].copy()
+
+    if df_slice.empty:
+        print(f"No data available for {year}-{month:02d}")
+        return
+
+    # Convert to numeric
+    df_slice['Wind speed in km/h'] = pd.to_numeric(df_slice['Wind speed in km/h'], 
+                                                    errors='coerce')
+    df_slice['Wind direction in degrees true'] = pd.to_numeric(df_slice['Wind direction in degrees true'],
+                                                               errors='coerce')
+    df_slice['Wind direction in degrees true'] %= 360
+
+    wind_speed = df_slice['Wind speed in km/h']
+    df_slice['Wind_norm'] = wind_speed / wind_speed.max()
+
+    speed_bins = [0, 10, 38, 60, 90,np.inf]
+    labels = ["Light", "Moderate", "Strong", "Severe", "Extreme"]
+
+
+    df_slice['speed_bin'] = pd.cut(df_slice['Wind speed in km/h'], bins=speed_bins, labels=labels)
+
+    sector_width = 5
+    df_slice['dir_bin'] = (df_slice['Wind direction in degrees true'] // sector_width) * sector_width
+
+    freq = df_slice.groupby(['dir_bin', 'speed_bin']).size().reset_index(name='count')
+    freq['percentage'] = 100 * freq['count'] / freq['count'].sum()
+
+    fig = px.bar_polar(freq, r="percentage", theta="dir_bin", color="speed_bin", color_continuous_scale=px.colors.sequential.Plasma)
+
+    fig.update_layout(title=f'Wind Rose at Station:{df_slice["Station Number"].iloc[0]} @ {year}-{month:02d}', 
+                      polar=dict(radialaxis=dict(tickformat=".0f%", ticksuffix="%",
+                      angle=90,
+                      side="counterclockwise")))
+    
+    fig.show()
